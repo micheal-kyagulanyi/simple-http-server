c32677e5ddb8fcee22d32f4ad5cc154468ffbc7c
mkyagulanyi-nbcuni <michael.kyagulanyi@nbcuni.com>
2024-05-13 22:14:25 -0500

    Initial Commit

---
+++ src/main/java/net/eziedev/httpserver/HttpServer.java
@@ -0,0 +1,13 @@
+package net.eziedev.httpserver;
+
+/**
+ *Driver class for the Http Server
+ *
+ */
+public class HttpServer {
+
+    public static void main( String[] args ) {
+
+        System.out.println( "Server starting..." );
+    }
+}
