d189630088357db555035635f1762fc369bb8292
mkyagulanyi-nbcuni <michael.kyagulanyi@nbcuni.com>
2024-05-13 23:17:57 -0500

    Adds and loads config file

--- src/main/java/net/eziedev/httpserver/HttpServer.java
+++ src/main/java/net/eziedev/httpserver/HttpServer.java
@@ -1,5 +1,8 @@
 package net.eziedev.httpserver;
 
+import net.eziedev.httpserver.config.Configuration;
+import net.eziedev.httpserver.config.ConfigurationManager;
+
 /**
  *Driver class for the Http Server
  *
@@ -9,5 +12,12 @@ public class HttpServer {
     public static void main( String[] args ) {
 
         System.out.println( "Server starting..." );
+
+        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+        Configuration configuration = ConfigurationManager.getInstance().getCurrentConfiguration();
+
+        System.out.println("Using port: " + configuration.getPort());
+        System.out.println("Using WebRoot: " + configuration.getWebroot());
+
     }
 }
